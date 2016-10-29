c79720daf9e284c3d43afca86eb2e5280b6ed679
Aalums <59070194@it.kmitl.ac.th>
2016-10-29 15:01:36 +0700

    bra bra

---
+++ Pro.py
@@ -0,0 +1,7 @@
+"""A​nalysis of teenage pregnancy"""
+def main():
+    """teenage pregnancy"""
+    read = open("E:/แอล/KMITL/PSIT/Project/data.txt", "r")
+    for i in read:
+        print(i)
+main()
