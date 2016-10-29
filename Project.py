9000b27cd3a0643938faa8d2bfa47d55178b3fb8
Anatda Planoi <59070184@it.kmitl.ac.th>
2016-10-29 15:16:44 +0700

    del s

--- Project.py
+++ Project.py
@@ -1,6 +1,6 @@
 """A​nalysis of teenage pregnancy"""
 def main():
-    """teenage pregnancy"""
+    """teenage pregnanc"""
     read = open("E:/แอล/KMITL/PSIT/Project/data.txt", "r")
     for i in read:
         print(i)
