4133566bde9d41cb05f3fd17612646d2150a34a4
Aalums <59070194@it.kmitl.ac.th>
2016-10-29 13:47:00 +0700

    Start

---
+++ Project.py
@@ -0,0 +1,7 @@
+"""A​nalysis of teenage pregnancy"""
+def main():
+    """A​nalysis of teenage pregnancy"""
+    read = open("E:/แอล/KMITL/PSIT/Project/data.txt", "r")
+    for i in read:
+        print(i)
+main()
