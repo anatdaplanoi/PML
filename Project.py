72f77fb74a5eefc8c444b20f8366ffc6344caa6d
Aalums <59070194@it.kmitl.ac.th>
2016-10-29 13:51:56 +0700

    Change docstring

--- Project.py
+++ Project.py
@@ -1,6 +1,6 @@
 """A​nalysis of teenage pregnancy"""
 def main():
-    """A​nalysis of teenage pregnancy"""
+    """teenage pregnancy"""
     read = open("E:/แอล/KMITL/PSIT/Project/data.txt", "r")
     for i in read:
         print(i)
