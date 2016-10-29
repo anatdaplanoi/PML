0c22b3bfe701637d331197d2063f0229e92a2f11
Anatda Planoi <59070184@it.kmitl.ac.th>
2016-10-29 14:20:01 +0700

    add we are PML

---
+++ Project.py
@@ -0,0 +1,8 @@
+"""A​nalysis of teenage pregnancy"""
+def main():
+    """teenage pregnancy"""
+    read = open("E:/แอล/KMITL/PSIT/Project/data.txt", "r")
+    for i in read:
+        print(i)
+    print("We are PML.")
+main()
